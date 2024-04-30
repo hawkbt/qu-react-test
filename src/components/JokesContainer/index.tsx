c637f127208cc1d3b20b021f5e4e185477b6a1b4
Jaseb Meza <jmeza792@gmail.com>
2024-04-30 11:51:17 -0300

    added Pagination

--- src/components/JokesContainer/index.tsx
+++ src/components/JokesContainer/index.tsx
@@ -8,6 +8,7 @@ import { ChevronLeft, ChevronRight } from '@mui/icons-material'
 
 const JokesContainer = () => {
   const [data, setData] = useState<Joke[]>([])
+  const [dataHolder, setDataHolder] = useState<Joke[]>([])
   const [page, setPage] = useState(1)
   const params = useSearchParams()
 
@@ -16,9 +17,28 @@ const JokesContainer = () => {
       const type = (params.get('type') || '') as Joke['type']
       const response = await getJokes({type})
       setData(response)
+      setDataHolder(response)
+      setPage(1)
     }
     getData()
   }, [params])
+
+  const onPageChange = async ( pageType: 'next' | 'prev') => {
+    
+    if (pageType === 'next'){
+      const type = (params.get('type') || '') as Joke['type']
+      const response = await getJokes({type})
+      const uniqueResponse = response.filter( (x: Joke)  => !dataHolder.some( i => i.id === x.id))
+      setData(uniqueResponse)
+      setDataHolder( d => [...d, ...uniqueResponse])
+    }else {
+      const start = page === 1 ? 0 : (page - 2) * 10
+      const end = page === 1 ? 10 : (page - 1) * 10
+      const response = dataHolder.slice(start, end)
+      setData([...response])
+    }
+    setPage( p => pageType === 'next' ? p + 1 : p - 1 === 0 ? 1 : p - 1)
+  }
 
   return (
     <Box>
@@ -30,11 +50,11 @@ const JokesContainer = () => {
         )}
       </Grid>
       <Stack flexWrap='nowrap' flexDirection='row' justifyContent='center' alignItems='center' gap={4} my={2}>
-        <Button variant='contained' disabled={page === 1}> 
+        <Button variant='contained' onClick={() => onPageChange('prev')} disabled={page === 1}> 
           <ChevronLeft/>
         </Button>
         <span>{page}</span>
-        <Button variant='contained' disabled={page === 1}>
+        <Button variant='contained' onClick={() => onPageChange('next')} disabled={data.length < 10}>
           <ChevronRight/>
         </Button>
       </Stack>
